93254d017d2a97e741be8d0e36c93f945c19022a
Adriano Anschau <adrianoanschau@gmail.com>
2023-07-25 23:05:50 -0300

    fix: adjusment on menus urls

--- config/leaguefy-admin.php
+++ config/leaguefy-admin.php
@@ -4,4 +4,25 @@ return [
     'route' => [
         'prefix' => '',
     ],
+    'menu' => [
+        [
+            'type' => 'sidebar-menu-search',
+            'text' => 'search',
+        ],
+        [
+            'text'        => 'games',
+            'url'         => 'games',
+            'icon'        => 'fas fa-fw fa-gamepad',
+        ],
+        [
+            'text'        => 'teams',
+            'url'         => 'teams',
+            'icon'        => 'fas fa-fw fa-users',
+        ],
+        [
+            'text'        => 'tournaments',
+            'url'         => 'tournaments',
+            'icon'        => 'fas fa-fw fa-trophy',
+        ],
+    ],
 ];
