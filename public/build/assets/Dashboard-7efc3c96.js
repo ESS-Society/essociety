781fbf75ba60805de4718d557b7638c43e81d1c7
Adriano Anschau <adrianoanschau@gmail.com>
2023-08-03 12:56:07 -0300

    update leaguefy-admin

--- public/build/assets/Dashboard-7efc3c96.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./app-3eb06faa.js";import{M as a}from"./Master-62527ced.js";function t(){return r(a,{children:"Dashboard"})}export{t as default};
---
+++ public/build/assets/Dashboard-c1be3d9b.js
@@ -0,0 +1,1 @@
+import{j as r}from"./app-cff39451.js";import{M as a}from"./Master-0f58515a.js";function t(){return r(a,{children:"Dashboard"})}export{t as default};
